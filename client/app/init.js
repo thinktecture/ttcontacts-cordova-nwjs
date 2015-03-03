971462037d873a88cddc5766bc2cee9f9e54729d
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-03 10:31:47 +0100

    Added Cordova project; with blank cordova.js for web app

---
+++ client/app/init.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+
+    function startAngularApp() {
+        var appContainer = document.querySelector('html');
+        angular.bootstrap(appContainer, ['starterApp']);
+    }
+
+    if (window.cordova) {
+        document.addEventListener('deviceready', startAngularApp, false);
+        alert("Cordova");
+    } else {
+        startAngularApp();
+    }
+})();
---
+++ client/cordova.js
@@ -0,0 +1,3 @@
+/**
+ * Created by christianweyer on 03.03.15.
+ */
