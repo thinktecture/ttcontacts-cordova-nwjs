d4824012f84f3a91c2aa9c8a0838487a8c23ef0d
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-03 16:54:18 +0100

    Added logic to export to file system

--- client/app/users/contacts/dummyContactsService.js
+++ client/app/users/contacts/dummyContactsService.js
@@ -3,7 +3,38 @@
 
     function ContactsService($log) {
         this.createContact = function (person) {
-            $log.debug('### NOT IMPLEMENTED!');
+            $log.debug('### Writing VCard');
+
+            var dlgName = 'exportFile';
+            var dlg = document.querySelector('#' + dlgName);
+
+            if (!dlg) {
+                var fileInput = document.createElement("input");
+                fileInput.setAttribute('type', 'file');
+                fileInput.setAttribute('nwsaveas', '');
+                fileInput.setAttribute('style', 'display:none;');
+                fileInput.setAttribute('id', dlgName);
+
+                dlg = document.querySelector('body').appendChild(fileInput);
+            }
+
+            saveFile(dlgName, 'foo - bar - baz');
+
+            function saveFile(name, data) {
+                var chooser = document.querySelector('#' + name);
+
+                chooser.addEventListener("change", function (evt) {
+                    var fs = require('fs');
+
+                    fs.writeFile(this.value, data, function (err) {
+                        if (err) {
+                            alert("error" + err);
+                        }
+                    });
+                }, false);
+
+                chooser.click();
+            }
         }
     }
 
