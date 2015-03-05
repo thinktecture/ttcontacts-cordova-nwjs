f3adadc0bbd51c5d4f4fdfbf1530b57aefa9c73d
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-05 10:50:19 +0100

    Small fixes

--- client/app/users/usersData.js
+++ client/app/users/usersData.js
@@ -20,6 +20,14 @@
                 content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
             },
             {
+                firstName: 'Marco',
+                lastName: 'Frodl',
+                email: 'marco.frodl@thinktecture.com',
+                phone: '+49 721 7540380',
+                avatar: 'svg-9',
+                content: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.'
+            },
+            {
                 firstName: 'Dominick',
                 lastName: 'Baier',
                 email: 'dominick.baier@thinktecture.com',
