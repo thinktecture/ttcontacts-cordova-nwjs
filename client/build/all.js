e64891e81c3bcb5cd58e3c1d2c2f8952b83ac24e
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-06 22:08:03 +0100

    Small updates

--- client/build/all.js
+++ client/build/all.js
@@ -110,6 +110,26 @@
 })();
 
 (function () {
+    "use strict";
+
+    /**
+     * @constructor
+     */
+    function MenuService() {
+        if (typeof process !== "undefined" && typeof require !== "undefined") {
+            var gui = require('nw.gui');
+
+            var window = gui.Window.get();
+            var nativeMenuBar = new gui.Menu({type: "menubar"});
+            nativeMenuBar.createMacBuiltin("TT Contacts");
+            window.menu = nativeMenuBar;
+        }
+    }
+
+    app.module.service('menuService', MenuService);
+})();
+
+(function () {
     'use strict';
 
     function UsersDataService($q) {
@@ -261,24 +281,4 @@
     UsersListController.$inject = ["usersDataService", "$mdSidenav", "$mdBottomSheet", "$log", "contactsService"];
 
     app.module.controller('UsersListController', UsersListController);
-})();
-
-(function () {
-    "use strict";
-
-    /**
-     * @constructor
-     */
-    function MenuService() {
-        if (typeof process !== "undefined" && typeof require !== "undefined") {
-            var gui = require('nw.gui');
-
-            var window = gui.Window.get();
-            var nativeMenuBar = new gui.Menu({type: "menubar"});
-            nativeMenuBar.createMacBuiltin("TT Contacts");
-            window.menu = nativeMenuBar;
-        }
-    }
-
-    app.module.service('menuService', MenuService);
 })();
