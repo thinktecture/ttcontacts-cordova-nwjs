f7aa16d45924974c6e22ceb8dd2ae0e043a468ea
Christian Weyer <christian.weyer@thinktecture.com>
2015-03-20 12:09:15 +0100

    Added icons and splash screen for Cordova

--- create-cordova-apps.sh
+++ create-cordova-apps.sh
@@ -4,11 +4,12 @@ mkdir cordova-build
 cd cordova-build
 
 cordova create TTContacts tt.com.ttc TTContacts --link-to=../client
-
 cd TTContacts
 
 cordova platform add ios
-
 cordova plugin add org.apache.cordova.contacts
+
+cp -r ../../client/assets/resources .
+ionic resources
 
 cordova prepare
