9f103bf60f12739dfdffa5074f46b51608a0976f
Christian Weyer <christian.weyer@thinktecture.com>
2015-05-04 21:13:48 +0200

    Updated build script

--- create-cordova-apps.sh
+++ create-cordova-apps.sh
@@ -7,7 +7,8 @@ cordova create TTContacts tt.com.ttc TTContacts --link-to=../client
 cd TTContacts
 
 cordova platform add ios
-cordova plugin add org.apache.cordova.contacts
+cordova platform add android
+cordova plugin add cordova-plugin-contacts
 
 cp -r ../../client/assets/resources .
 ionic resources
